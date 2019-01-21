dc0a1678980e8d961092ad928c96cd18e01f5aa7
wyyx <gnehcyx@163.com>
2019-01-21 19:27:44 +0800

    add navigation and containers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,42 +1,44 @@
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser'
+import { NgModule } from '@angular/core'
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button'
-import {MatIconModule} from '@angular/material/icon';
-import {MatCardModule} from '@angular/material/card';
-import { HomeComponent } from './home/home.component';
-import { AboutComponent } from './about/about.component';
-import {MatTabsModule} from '@angular/material/tabs';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
-import {MatToolbarModule} from '@angular/material/toolbar';
-
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from './app.component'
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { MatMenuModule } from '@angular/material/menu'
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
+import { MatCardModule } from '@angular/material/card'
+import { HomeComponent } from './home/home.component'
+import { AboutComponent } from './about/about.component'
+import { MatTabsModule } from '@angular/material/tabs'
+import { TopMenuComponent } from './top-menu/top-menu.component'
+import { CoursesCardListComponent } from './courses-card-list/courses-card-list.component'
+import { MatSidenavModule } from '@angular/material/sidenav'
+import { MatListModule } from '@angular/material/list'
+import { MatToolbarModule } from '@angular/material/toolbar'
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        HomeComponent,
-        AboutComponent
-    ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        MatMenuModule,
-        MatButtonModule,
-        MatIconModule,
-        MatCardModule,
-        MatTabsModule,
-        AppRoutingModule,
-        MatSidenavModule,
-        MatListModule,
-        MatToolbarModule
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    TopMenuComponent,
+    CoursesCardListComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCardModule,
+    MatTabsModule,
+    AppRoutingModule,
+    MatSidenavModule,
+    MatListModule,
+    MatToolbarModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {
-}
+export class AppModule {}
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,29 +1,24 @@
-import {Component, OnInit} from '@angular/core';
-import {Course} from "../model/course";
-import {COURSES} from "../model/db-data";
+import { Component, OnInit } from '@angular/core'
+import { Course } from '../model/course'
+import { COURSES } from '../model/db-data'
 
 @Component({
-    selector: 'home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: 'home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  beginnerCourses: Course[]
 
-    beginnerCourses: Course[];
+  advancedCourses: Course[]
 
-    advancedCourses: Course[];
+  constructor() {}
 
-    constructor() {
+  ngOnInit() {
+    const courses = <any>Object.values(COURSES)
 
-    }
+    this.beginnerCourses = courses.filter(course => course.category === 'BEGINNER')
 
-    ngOnInit() {
-
-        const courses:any = Object.values(COURSES);
-
-        this.beginnerCourses = courses.filter(course => course.category === 'BEGINNER');
-
-        this.advancedCourses = courses.filter(course => course.category === 'ADVANCED');
-    }
-
+    this.advancedCourses = courses.filter(course => course.category === 'ADVANCED')
+  }
 }
