452db768d66eff4b0f57135c2d5f2dc82e857288
wyyx <gnehcyx@163.com>
2019-01-21 15:25:29 +0800

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatMenuModule} from '@angular/material/menu';
+import {MatButtonModule} from '@angular/material/button'
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatListModule} from '@angular/material/list';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        HomeComponent,
+        AboutComponent
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        MatMenuModule,
+        MatButtonModule,
+        MatIconModule,
+        MatCardModule,
+        MatTabsModule,
+        AppRoutingModule,
+        MatSidenavModule,
+        MatListModule,
+        MatToolbarModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule {
+}
